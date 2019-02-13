269e8fdbe0e0ef6a8c82d54404cc82674ac512f2
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-13 14:49:55 -0500

    Add Inventory handler added to InventoryPage.js and passed to AddInventory.js on props

--- souper-douper/src/components/InventoryMgmt/InventoryPage.js
+++ souper-douper/src/components/InventoryMgmt/InventoryPage.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 //components
 import NavBar from "../NavBar/NavBar";
 import InventoryView from "./InventoryView";
+import AddInventory from "./AddInventory";
 import Item from "../ItemsList/Item";
 import EditItem from "../ItemsList/EditItem";
 //styles
@@ -31,6 +32,33 @@ class InventoryPage extends React.Component {
       })
       .catch(err => console.log(err));
   }
+  // pass below function to AddInventory.js as addNewItem on props
+  addItemHandler = e => {
+    e.preventDefault();
+    let newItem = {
+      name: e.target[0].value,
+      amount: parseInt(e.target[1].value),
+      unit: e.target[2].value,
+      imageUrl: e.target[3].value,
+      categoryID: e.target[4].value
+    };
+    let auth = {
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    };
+    axios
+      .post(
+        "https://soup-kitchen-backend.herokuapp.com/api/items",
+        newItem,
+        auth
+      )
+      .then(res => {
+        this.setState({ items: res.data.items });
+      })
+      .catch(err => console.log(err));
+    e.target.reset();
+  };
 
   logUserOut = () => {
     localStorage.clear();
@@ -46,6 +74,12 @@ class InventoryPage extends React.Component {
           path="/"
           render={props => (
             <InventoryView {...props} items={this.state.items} />
+          )}
+        />
+        <Route
+          path="/add"
+          render={props => (
+            <AddInventory {...props} addNewItem={this.addItemHandler} />
           )}
         />
       </div>
